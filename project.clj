ed9d309523b05fbae019fbaedd484a7d9099cacf
Tobias Bayer <tobayer@googlemail.com>
2014-10-31 13:33:42 +0100

    Add test for registering

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-http "1.0.0"]]
-  :profiles {:dev {:dependencies [[org.clojure/data.json "0.2.5"]]
+  :profiles {:dev {:dependencies [[midje "1.6.3"]]
                    :plugins [[lein-midje "3.1.3"]]}})
--- test/eureka_client/core_test.clj
+++ test/eureka_client/core_test.clj
@@ -1,7 +1,23 @@
 (ns eureka-client.core-test
-  (:require [clojure.test :refer :all]
-            [eureka-client.core :refer :all]))
+  (:require [midje.sweet :refer :all]
+            [eureka-client.core :refer :all]
+            [clj-http.client :as http]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(fact "it registers the app"
+  (register "localhost" 8761 "myId" 1234) => anything
+    (provided
+      (http/post
+        "http://localhost:8761/eureka/apps/myId"
+        (contains {:form-params {:instance {:hostName (.getHostAddress (java.net.InetAddress/getLocalHost))
+                                  :app "myId"
+                                  :ipAddr (.getHostAddress (java.net.InetAddress/getLocalHost))
+                                  :vipAddress "myId"
+                                  :status "UP"
+                                  :port 1234
+                                  :securePort 443
+                                  :dataCenterInfo {:name "MyOwn"}}}})) => anything))
+
+(fact "it sends heartbeats")
+
+
