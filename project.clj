d4312db2cae23fd7a63325e9440dffe2d523a119
Tobias Bayer <tobayer@googlemail.com>
2014-11-07 15:01:00 +0100

    Increase version number for dev

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject eureka-client "0.1.1"
+(defproject eureka-client "0.2.0-SNAPSHOT"
   :description "A client for Netflix Eureka service discovery servers"
   :url "http://github.com/codebrickie/eureka-client"
   :license {:name "Eclipse Public License"
