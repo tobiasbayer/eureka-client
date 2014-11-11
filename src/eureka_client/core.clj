31c572b01747ed83cc5d00c69ba10d585268a23d
Tobias Bayer <tobayer@googlemail.com>
2014-11-11 16:39:56 +0100

    Generate a unique host id

--- src/eureka_client/core.clj
+++ src/eureka_client/core.clj
@@ -14,30 +14,35 @@
         (Thread/sleep ms)
         (recur)))))
 
+(defn- eureka-path [host port]
+  (str "http://" host ":" port "/eureka/apps"))
+
 (defn- app-path [host port app-id]
-  (str "http://" host ":" port "/eureka/apps/" app-id))
+  (str (eureka-path host port) "/" app-id))
 
 (defn- instance-path [host port app-id instance-id]
   (str (app-path host port app-id) "/" instance-id))
 
-(defn- send-heartbeat [host port app-id]
-  (http/put (instance-path host port app-id (.getHostAddress (java.net.InetAddress/getLocalHost)))
+(defn- send-heartbeat [host port app-id host-id]
+  (http/put (instance-path host port app-id host-id)
             request-opts))
 
 (defn register
   "Registers the app-id at the Eureka server and sends a heartbeat every 30 seconds"
   [eureka-host eureka-port app-id own-port]
   (do
-    (http/post (app-path eureka-host eureka-port app-id)
-               (assoc
-                 request-opts
-                 :form-params
-                 {:instance {:hostName (.getHostAddress (java.net.InetAddress/getLocalHost))
-                             :app app-id
-                             :ipAddr (.getHostAddress (java.net.InetAddress/getLocalHost))
-                             :vipAddress app-id
-                             :status "UP"
-                             :port own-port
-                             :securePort 443
-                             :dataCenterInfo {:name "MyOwn"}}}))
-    (tick 30000 send-heartbeat eureka-host eureka-port app-id)))
+    (let [host-id (str (.getHostAddress (java.net.InetAddress/getLocalHost)) "-" (rand-int 10000))]
+      (http/post (app-path eureka-host eureka-port app-id)
+                 (assoc
+                   request-opts
+                   :form-params
+                   {:instance {:hostName host-id
+                               :app app-id
+                               :ipAddr (.getHostAddress (java.net.InetAddress/getLocalHost))
+                               :vipAddress app-id
+                               :status "UP"
+                               :port own-port
+                               :securePort 443
+                               :dataCenterInfo {:name "MyOwn"}}}))
+      (tick 30000 send-heartbeat eureka-host eureka-port app-id host-id))))
+
--- test/eureka_client/core_test.clj
+++ test/eureka_client/core_test.clj
@@ -7,12 +7,14 @@
 (fact "it registers the app"
   (register "localhost" 8761 "myId" 1234) => anything
     (provided
+      (rand-int 10000) => 1
       (http/post
         "http://localhost:8761/eureka/apps/myId"
-        (contains {:form-params {:instance {:hostName (.getHostAddress (java.net.InetAddress/getLocalHost))
-                                  :app "myId"
-                                  :ipAddr (.getHostAddress (java.net.InetAddress/getLocalHost))
-                                  :vipAddress "myId"
+        (contains {:form-params {:instance
+                                  {:hostName (str (.getHostAddress (java.net.InetAddress/getLocalHost)) "-" 1)
+                                   :app "myId"
+                                   :ipAddr (.getHostAddress (java.net.InetAddress/getLocalHost))
+                                   :vipAddress "myId"
                                   :status "UP"
                                   :port 1234
                                   :securePort 443
