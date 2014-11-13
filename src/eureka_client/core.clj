78e76817a51203cf5b5d7b540cad4a4c2ac873aa
Tobias Bayer <tobayer@googlemail.com>
2014-11-13 12:22:53 +0100

    Remove superfluous "do"

--- src/eureka_client/core.clj
+++ src/eureka_client/core.clj
@@ -13,10 +13,9 @@
 (defn- tick [ms f & args]
   (future
     (loop []
-      (do
-        (apply f args)
-        (Thread/sleep ms)
-        (recur)))))
+      (apply f args)
+      (Thread/sleep ms)
+      (recur))))
 
 (defn- eureka-path [host port]
   (str "http://" host ":" port "/eureka/apps"))
@@ -34,9 +33,8 @@
 (defn register
   "Registers the app-id at the Eureka server and sends a heartbeat every 30 seconds"
   [eureka-host eureka-port app-id own-port]
-  (do
-    (let [host-id (str (.getHostAddress (java.net.InetAddress/getLocalHost)) "-" (rand-int 10000))]
-      (http/post (app-path eureka-host eureka-port app-id)
+  (let [host-id (str (.getHostAddress (java.net.InetAddress/getLocalHost)) "-" (rand-int 10000))]
+    (http/post (app-path eureka-host eureka-port app-id)
                  (assoc
                    request-opts
                    :form-params
@@ -48,7 +46,7 @@
                                :port own-port
                                :securePort 443
                                :dataCenterInfo {:name "MyOwn"}}}))
-      (tick 30000 send-heartbeat eureka-host eureka-port app-id host-id))))
+      (tick 30000 send-heartbeat eureka-host eureka-port app-id host-id)))
 
 (defn- fetch-instances-from-server
   [eureka-host eureka-port app-id]
